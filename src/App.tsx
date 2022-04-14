0c8edf07f6cc5f9fb61bf717d8bc929efdf3d3dc
Marmik Patel <marmikpatel250@gmail.com>
2022-04-14 22:05:03 +0530

    commented unused elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import BaseLayout from "./components/BaseLayout";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
+// import Login from "./pages/Login";
+// import Register from "./pages/Register";
 
 function App() {
 
